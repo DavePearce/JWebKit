1f760b2d73adff7b7ef0d4d365e86f0785364a08
DavePearce <david.pearce@ecs.vuw.ac.nz>
2015-12-15 16:36:26 +1300

    Working towards useful db interface

--- src/jwebkit/sql/SqlTable.java
+++ src/jwebkit/sql/SqlTable.java
@@ -1,5 +1,7 @@
 package jwebkit.sql;
 
+import java.util.Iterator;
+
 /**
  * Respresents an SQL table as accessed via JDBC. The purpose of this class is
  * to simplify the process of communicating with the table.
@@ -8,24 +10,65 @@ package jwebkit.sql;
  *
  */
 public class SqlTable {
+	/**
+	 * The table name
+	 */
+	private String name;
 	
 	/**
-	 * Represents a column in the table schema
-	 * @author David J. Pearce
-	 *
+	 * The schema for the table
 	 */
-	public static class Column {
-		private String name;
-		private SqlType type;
-	}	
+	private SqlColumn[] schema;
 	
-	/**
-	 * Represents a 32bit twos-complement integer.
-	 * 
-	 * @author djp
-	 *
-	 */
-	private static class Integer extends Column {
+	public SqlTable(String name, SqlColumn... schema) {
+		this.name = name;
+		this.schema = schema;
+	}
+
+	public SqlColumn getColumn(int index) {
+		return schema[index];
+	}
+	
+	public SqlColumn getColumn(String name) {
+		for(int i=0;i!=schema.length;++i) {
+			SqlColumn c = schema[i];
+			if(c.getName().equals(name)) {
+				return c;
+			}
+		}
+		throw new IllegalArgumentException("Invalid column - " + name);
+	}
+	
+	public int nColumns() {
+		return schema.length;
+	}
+	
+	public Iterator<SqlRow> iterator() {
+		return new SqlTableIterator();
+	}
 		
+	private class SqlTableIterator implements Iterator<SqlRow> {
+		
+		public SqlTableIterator() {
+			
+		}
+
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public SqlRow next() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
 	}
 }
