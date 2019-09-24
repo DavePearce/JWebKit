c603136161ba1c046c60a4962a3f1086813a2915
DavePearce <david.pearce@ecs.vuw.ac.nz>
2019-09-24 18:04:54 +1200

    Update HttpFileHandler to support POST
    This updates the HttpFileHandler to support POST requests which write files to a
    given directory.

--- src/main/java/jwebkit/http/HttpFileHandler.java
+++ src/main/java/jwebkit/http/HttpFileHandler.java
@@ -7,12 +7,16 @@ import static java.net.HttpURLConnection.HTTP_OK;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
@@ -68,4 +72,53 @@ public class HttpFileHandler extends HttpMethodDispatchHandler {
 			response.setStatusCode(HttpStatus.SC_BAD_REQUEST);
 		}
 	}
+
+	@Override
+	protected void post(HttpRequest request, HttpResponse response, HttpContext context)
+			throws HttpException, IOException {
+		if (request instanceof HttpEntityEnclosingRequest) {
+			post((HttpEntityEnclosingRequest) request, response, context);
+		} else {
+			response.setStatusCode(HttpStatus.SC_BAD_REQUEST);
+		}
+	}
+
+	protected void post(HttpEntityEnclosingRequest request, HttpResponse response, HttpContext context) {
+		try {
+			String uri = request.getRequestLine().getUri();
+			String path = new URIBuilder(uri).getPath();
+			HttpEntity entity = request.getEntity();
+			// Ensure matching mime types. Whilst not really a strong safeguard, it's
+			// nevertheless a good idea.
+			if (mimeType.getMimeType().equals(entity.getContentType().getValue())) {
+				File file = new File(rootDir, path);
+				writeFile(entity.getContent(), file);
+				response.setStatusCode(HttpStatus.SC_OK);
+				return;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		response.setStatusCode(HttpStatus.SC_BAD_REQUEST);
+	}
+
+	/**
+	 * Write a given file in chunks to the output stream.
+	 *
+	 * @param out
+	 * @param file
+	 * @throws IOException
+	 */
+	private void writeFile(InputStream in, File file) throws IOException {
+		FileOutputStream fout = new FileOutputStream(file);
+		byte[] bytes = new byte[CHUNK_SIZE];
+		int nread;
+		while ((nread = in.read(bytes, 0, CHUNK_SIZE)) == CHUNK_SIZE) {
+			fout.write(bytes, 0, CHUNK_SIZE);
+		}
+		fout.write(bytes, 0, nread);
+		fout.close();
+		in.close();
+	}
+
 }
